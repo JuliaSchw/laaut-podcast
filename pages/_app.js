a5ad1ecc0f466344060c50150de639e6672d5d4b
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-02-03 16:40:30 +0100

    implement next google font roboto flex

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import GlobalStyle from "../styles";
+import { Roboto_Flex } from "next/font/google";
+
+const roboto = Roboto_Flex({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
